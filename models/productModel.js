0131ad20b88cc4b30ed06d9e824800f388b02305
chamcbs <chamudithacbs2@gmail.com>
2022-12-06 17:23:22 +0530

    complete admin dashboard

--- models/productModel.js
+++ models/productModel.js
@@ -9,10 +9,10 @@ const productSchema = new mongoose.Schema(
         category: { type: String, required: true },
         description: { type: String, required: true },
         price: { type: Number, required: true },
-        rent: { type: Number, required: true },
+        rent: { type: Number, required: false },
         countInStock: { type: Number, required: true },
-        rating: { type: Number, required: true },
-        numReviews: { type: Number, required: true },
+        rating: { type: Number, required: false },
+        numReviews: { type: Number, required: false },
 
     },
     {
